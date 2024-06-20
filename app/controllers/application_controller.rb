39cbdce041e12506ef4dae6e713791f61bac2464
kwo <konstantin@edisonlf.com>
2024-06-20 11:27:13 +0300

    user registration finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :check_user_authentication
+
+  private
+
+  def check_user_authentication
+    return if user_signed_in?
+    return if bypass_authentication_for_guest_views?
+
+    redirect_to root_url
+  end
+
+  def bypass_authentication_for_guest_views?
+    # Ensure registration paths are allowed
+    return true if controller_path == 'devise/registrations' && action_name.in?(['new', 'create'])
+    return true if controller_path == 'devise/sessions' && action_name == 'new'
+    return true if controller_name == 'home' && action_name == 'index'
+
+    false
+  end
 end
