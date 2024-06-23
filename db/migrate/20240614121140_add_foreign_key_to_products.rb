b4de94a2e6bcffe1725df125fdf78215a7bea3d4
kwo <konstantin@edisonlf.com>
2024-06-23 14:47:41 +0300

    fixed the migration file

--- db/migrate/20240614121140_add_foreign_key_to_products.rb
+++ db/migrate/20240614121140_add_foreign_key_to_products.rb
@@ -1,5 +1,8 @@
 class AddForeignKeyToProducts < ActiveRecord::Migration[7.1]
   def change
-    add_foreign_key :products, :defendants, column: :defendant_id
+    # Check if the foreign key constraint already exists
+    unless foreign_key_exists?(:products, :defendants, column: :defendant_id)
+      add_foreign_key :products, :defendants, column: :defendant_id
+    end
   end
 end
